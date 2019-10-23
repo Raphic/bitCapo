27890712736e2ca2ff45e9f5db37c74032a1d15c
Homez <tndkhoa1104@yahoo.com>
2019-10-23 18:56:10 +0700

    update backup new

---
+++ js/bk/script.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+    var swiper = new Swiper('.rateTerm', {
+        slidesPerView: 3,
+        spaceBetween: 80,
+        breakpoints: {
+            1200: {
+              spaceBetween: 40,
+            },
+            992: {
+              slidesPerView: 2,
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            576: {
+              slidesPerView: 1,
+              spaceBetween: 20,
+            },
+        },
+        scrollbar: {
+            el: '.swiper-scrollbar',
+            hide: true,
+        },
+    });
+    
+    $('#hisOrderTable').DataTable({
+        "bLengthChange": false,
+        "bFilter": true,
+        searching: false
+    });
+    
+    $('#priceAlerts').DataTable({
+        "bLengthChange": false,
+        "bFilter": true,
+        searching: false
+    });
+    
+    $('#portfolioBuy').DataTable({
+        "bLengthChange": false,
+        "bFilter": true,
+        searching: false,
+        "info": false
+    });
+    $('#portfolioSell').DataTable({
+        "bLengthChange": false,
+        "bFilter": true,
+        searching: false,
+        "info": false
+    });
+    
+});
+
+
